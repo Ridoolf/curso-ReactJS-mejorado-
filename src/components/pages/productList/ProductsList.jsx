921ce83c789d4a8085d355ae016c531b5c5716d4
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2024-07-23 11:14:18 -0300

    como se sentencia una promesa + then - catch + async - await - simulando promesa con setTimeOut

--- src/components/pages/productList/ProductsList.jsx
+++ src/components/pages/productList/ProductsList.jsx
@@ -1,8 +1,3 @@
-export const ProductsList = ({ contador, setContador }) => {
-  return (
-    <>
-      <h1>Aca van los productos</h1>
-      <button onClick={() => setContador(contador + 1)}>Sumar</button>
-    </>
-  );
+export const ProductsList = () => {
+  return <></>;
 };
--- src/components/pages/productList/ProductsListContainer.jsx
+++ src/components/pages/productList/ProductsListContainer.jsx
@@ -1,14 +1,67 @@
 import { useEffect, useState } from "react";
-import { ProductsList } from "./ProductsList";
+import { products } from "../../../productsMock";
 
 export const ProductsListContainer = () => {
-  const [contador, setContador] = useState(0);
+  // Ejemplo 2
 
-  console.log("El componente fue montado");
+  const [items, setItems] = useState([]);
+  console.log(items);
 
+  // recordemos que la peticion a la API debe estar dentro de un useEffect para que no se realice cada vez que se actualiza el componente
   useEffect(() => {
-    console.log("Realizamos la peticion a la API");
+    const tarea = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(products);
+      }, 3000); // --> Como las peticiones suelen tener un minimo de demora aleatorio (1 segundo, 2, 4, 5 milesimas, etc) simulamos esta demora con un setTimeOut
+    });
+
+    // Ejemplo con then & catch
+    tarea
+      .then((res) => {
+        setItems(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // Ejemplo 1
+
+    // // para crear una promesa
+    // const tarea = new Promise((resolve, reject) => {
+    //   // la promesa recibe 2 parametros, resolve para cuando la promesa se resuelve de manera correcta, y reject para cuando no se resuelve
+    //   resolve("la promesa se resolvio");
+    // }); // --> Para este ejemplo crearemos una promesa en el front y la resolveremos en el front (cosa que en un caso real no tiene sentido y solamente es por fines practicos)
+
+    // tarea
+    //   .then((res) => {
+    //     console.log("then: ", res);
+    //   }) // toma el resolve de la promesa
+    //   .catch((err) => {
+    //     console.log("catch: ", err);
+    //   }); // toma el reject de la promesa
+
+    // // Ejemplo con async & await
+
+    // const getDatos = async () => {
+    //   // // creamos una funcion con la palabra reservada 'async'
+    //   // let res = await tarea; // especificamos a que promesa hay que 'esperar'
+    //   // setFrase(res); // una vez resuelta la promesa seteamos el resolve en la frase
+
+    //   // forma correcta para el manejo de errores
+    //   try {
+    //     // intenta hacer esto
+    //     let res = await tarea;
+    //     setFrase(res);
+    //   } catch (err) {
+    //     // si no se puede hace esto (algo asi como un if...else)
+    //     console.log(err);
+    //   }
+    // };
+
+    // getDatos(); // ejecutamos la funcion
   }, []);
 
-  return <ProductsList contador={contador} setContador={setContador} />;
+  return;
 };
+
+// una promesa es el resultado de la peticion a una API
