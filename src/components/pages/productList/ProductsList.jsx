ffaa63f7d21176620639a752fe795c3a44fbdec3
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2024-07-23 12:36:00 -0300

    estilando con mui

--- src/components/pages/productList/ProductsList.jsx
+++ src/components/pages/productList/ProductsList.jsx
@@ -1,11 +1,12 @@
 import ProductCard from "../../common/card/ProductCard";
+import "./ProductsList.css";
 
 export const ProductsList = ({ items }) => {
   return (
-    <>
+    <div className="cards_container">
       {items.map((item) => {
         return <ProductCard key={item.id} item={item} />;
       })}
-    </>
+    </div>
   );
 };
