36410af0d358445c8d36eeaa72fe3cc736dc414f
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2024-07-23 11:16:07 -0300

    mock de productos que simula nuestro backend

---
+++ src/productsMock.js
@@ -0,0 +1,15 @@
+// vamos a simular las solicitudes a nuestros productos en la nube... mas adelante esto sera mediante el backend
+
+export const products = [
+
+    {
+        id: 1,
+        title: "zapatillas nike airMax",
+        description: "estas son las mejores airMax del mercado papaa",
+        price: 1000,
+        stock: 9,
+        img: "url de imagen",
+        category: "deportivas",
+    }
+
+]
