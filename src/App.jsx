ac8531c839830270a9d5e2945fdfb461b99be56a
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2024-08-05 11:00:05 -0300

    concepto de childrens + logica de add to cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@ import { Navbar } from "./components/layout/navbar/Navbar";
 // import { ProductsListContainer } from "./components/pages/productList/ProductsListContainer";
 import "./App.css";
 import "@fontsource/roboto/300.css";
-import { Users } from "./components/Users";
-import { Comments } from "./components/Comments";
-import { Post } from "./components/Post";
-// import { ProductDetailContainer } from "./components/pages/productDetail/ProductDetailContainer";
+// import { Users } from "./components/Users";
+// import { Comments } from "./components/Comments";
+// import { Post } from "./components/Post";
+import { ProductDetailContainer } from "./components/pages/productDetail/ProductDetailContainer";
 
 function App() {
   return (
     <div className="app-container">
-      <header>
-        <Navbar />
-      </header>
-      {/* <ProductsListContainer />
-      <ProductDetailContainer /> */}
-      <Users />
-      <Comments />
-      <Post />
+      <Navbar>
+        {/* <ProductsListContainer /> */}
+        <ProductDetailContainer />
+        {/* <Users /> */}
+        {/* <Comments /> */}
+        {/* <Post /> */}
+      </Navbar>
     </div>
   );
 }
