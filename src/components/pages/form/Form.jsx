11ee5db8767c63a846059fa9d59c77266c10b563
Lucas Ridolfi <lucasg.ridolfi@gmail.com>
2024-08-26 09:17:53 -0300

    limpiando archivos y comentarios de la clase 10

--- src/components/pages/form/Form.jsx
+++ src/components/pages/form/Form.jsx
@@ -6,21 +6,13 @@ export const Form = () => {
     lastname: "",
     email: "",
   });
-  // En vez d crear un estado por cada input, creamos un estado que contenga un objeto con los datos del usuario
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // antes de comenzar con la logica del handle es donde se colocan las
-    // validaciones para los campos
-
     if (!userData.email.includes("@")) {
       console.log("El campo email no es valido");
       return;
-    }
-
-    if (userData.name.length <= 3) {
-      console.log("El nombre es muy corto");
     }
 
     let data = {
@@ -32,27 +24,7 @@ export const Form = () => {
   };
 
   const handleData = (e) => {
-    {
-      /*
-        Aca tambien podemos colocar validaciones pero hay que tener en cuenta 
-        que cada vez el usuario escriba algo, el handle va a validar el input
-        Por ejemplo, si aca ponemos la validacion de que el name sea mayor a 3 letras,
-        ni bien el usuario ponga la primer letra de su nombre ya va a devolver un
-        error
-      */
-    }
-
     setUserData({ ...userData, [e.target.name]: e.target.value });
-    {
-      /*
-        Ahora, como queremos que esto sea escalable creamos un solo handle que en vez de tener hardcodeada la propiedad
-        que queremos cambiar por nuestro value del input, tomamos el nombre del input y le decimos que coloque el target.value
-        en el target.name que se este modificando. Es importante destacar que cuando creemos el input en nuestro html, en la
-        propiedad name debemos poner la propiedad que queremos cambiar, ya que nuestro target.name toma ese valor... 
-        Por ultimo, realizamos un spread del objeto userData, para que no se modifique de manera erronea el objeto y respete los
-        datos del usario anteriormente creados
-    */
-    }
   };
 
   return (
@@ -82,54 +54,3 @@ export const Form = () => {
     </div>
   );
 };
-
-// Forma 1 (No escalable)
-// --> Que sucede? Si por cada input tenemos que crear un estado y un handle se hace muy tedioso el hecho de crear
-// formularios...
-// export const Form = () => {
-//   const [name, setName] = useState("");
-//   const [lastname, setLastname] = useState("");
-
-//   const handleSubmit = (e) => {
-//     {
-//       /* Handle: Por convencion a las funciones asignadas a los form se les llama handleAlgoMas */
-//       /* Event: Al llamar una funcion desde un onSubmit, react nos va a enviar un parametro por default, a este parametro
-//       habitualmente se lo llama evento / event / e */
-//     }
-//     e.preventDefault();
-//     let data = {
-//       nombreUsuario: name,
-//       apellidoUsuario: lastname,
-//     };
-//     console.log(data);
-//   };
-
-//   const handleName = (e) => {
-//     setName(e.target.value);
-//   };
-
-//   const handleLastname = (e) => {
-//     setLastname(e.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <h2>Este es el form</h2>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Ingrese su nombre"
-//           name="nombre"
-//           onChange={handleName}
-//         />
-//         <input
-//           type="text"
-//           placeholder="Ingrese su apellido"
-//           name="apellido"
-//           onChange={handleLastname}
-//         />
-//         <button>Enviar</button> {/* Por default es type submit */}
-//       </form>
-//     </div>
-//   );
-// };
--- src/components/pages/productDetail/ProductDetailContainer.jsx
+++ src/components/pages/productDetail/ProductDetailContainer.jsx
@@ -22,23 +22,3 @@ export const ProductDetailContainer = () => {
 
   return <ProductDetail productSelected={productSelected} />;
 };
-
-/* UseParams
-
-El use params toma lo que nosotros pongamos como dinamico en la ruta
-Por ejemplo, en nuestro app tenemos:
-
-  <Route path="/itemDetail/:id" element={<ProductDetailContainer />} />
-
-Ese :id indica que el itemDetail va a tener un /algo... que puede ser cualquier cosa, a esa ruta dinamica
-la llamamos id
-
-El use params toma ese valor y nos devuelve un objeto el cual desesctructuramos y llamamos id
-
-  const { id } = useParams();
-
-Pero que sucede? 
-  Este id viene desde el useParams en formato string, y nosotros en el .find estamos
-intentando comparar un valor numerico id al string id de nuestro useParams, por lo que hay que convertirlo a 
-string... eso es tan simple como colocarle un + antes de llamarlo: +id
-*/
